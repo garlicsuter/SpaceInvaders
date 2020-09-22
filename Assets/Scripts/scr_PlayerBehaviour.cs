c7382d9f4fbac35f1d5037201b570d9dfad32c83
garlicsuter <markasuter@gmail.com>
2020-09-22 11:01:35 -0400

    move player

---
+++ Assets/Scripts/scr_PlayerBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_PlayerBehaviour : MonoBehaviour
+{
+    private Transform playerTransform;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTransform = GetComponent<Transform>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        Vector3 movement = new Vector3(moveHorizontal, 0);
+        playerTransform.position += movement * speed;
+    }
+}
