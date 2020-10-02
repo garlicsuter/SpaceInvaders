98fa53dcfdc4f1493e73a085ff9c2bee429de9e8
garlicsuter <markasuter@gmail.com>
2020-10-02 10:57:55 -0400

    enemyMovement

--- Assets/Scripts/scr_Enemy.cs
+++ Assets/Scripts/scr_Enemy.cs
@@ -6,16 +6,29 @@ public class scr_Enemy : MonoBehaviour
 {
     private Transform enemyTransform;
     public float enemySpeed;
+    private int enemyDirection = 1;
+
+
     // Start is called before the first frame update
     void Start()
     {
         enemyTransform = GetComponent<Transform>();
+
+        InvokeRepeating("moveEnemy", 1.0f, 0.5f);
     }
 
+    void moveEnemy()
+    {
+        if (enemyDirection == 1)
+        {
+            enemyTransform.position += new Vector3(1.0f, 0.0f);
+            Debug.Log("I moved right 1");
+        }
+    }
     // Update is called once per frame
     void Update()
     {
-        enemyTransform.position += Vector3.down * enemySpeed * Time.deltaTime;
+        //enemyTransform.position += Vector3.down * enemySpeed * Time.deltaTime;
     }
 
     
